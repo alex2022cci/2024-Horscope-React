a4f8e03a4a9b1d57ab355e7e122ca661217d7e66
alex2022cci <alex2022cci@outlook.fr>
2024-06-02 18:17:39 +0200

    ajout des signes sur l'horloge

--- src/App.jsx
+++ src/App.jsx
@@ -24,6 +24,18 @@ const ZodiacClock = ({ angle }) => {
       <img src="/roue.png" alt="Zodiac Circle" className="zodiac-image img-fluid" />
       { <div className='center'></div>}
       {angle !== null && <div className="needle" style={{ transform: `rotate(${angle}deg)` }} />}
+      <img src="/Bélier.png" alt="Bélier Zodiac Sign" className="belier" />
+      <img src="/Taureau.png" alt="Taureau Zodiac Sign" className="taureau" />
+      <img src="/Gémeaux.png" alt="Gémeaux Zodiac Sign" className="gemeaux" />
+      <img src="/Cancer.png" alt="Cancer Zodiac Sign" className="cancer" />
+      <img src="/Lion.png" alt="Lion Zodiac Sign" className="lion" />
+      <img src="/Vierge.png" alt="Vierge Zodiac Sign" className="vierge" />
+      <img src="/Balance.png" alt="Balance Zodiac Sign" className="balance" />
+      <img src="/Scorpion.png" alt="Scorpion Zodiac Sign" className="scorpion" />
+      <img src="/Sagittaire.png" alt="Sagittaire Zodiac Sign" className="sagittaire" />
+      <img src="/Capricorne.png" alt="Capricorne Zodiac Sign" className="capricorne" />
+      <img src="/Verseau.png" alt="Verseau Zodiac Sign" className="verseau" />
+      <img src="/Poissons.png" alt="Poissons Zodiac Sign" className="poissons" />
     </div>
   );
 };
